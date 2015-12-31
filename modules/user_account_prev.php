0484a34f2e27c64687c92dce11b062ef7cc3a529
Joshua Nasiatka <joshuanasiatka@gmail.com>
2015-12-30 22:57:59 -0500

    Fixed Profile Button Bug
    Bug fixed pertaining to button not working outside of the root of the
    domain.

--- modules/user_account_prev.php
+++ modules/user_account_prev.php
@@ -33,7 +33,7 @@
     <!-- Menu Footer-->
     <li class="user-footer">
       <div class="pull-left">
-        <a href="?page=profile" class="btn btn-default btn-flat">Profile</a>
+        <a href="/?page=profile" class="btn btn-default btn-flat">Profile</a>
       </div>
       <div class="pull-right">
         <a href="logout.php" class="btn btn-default btn-flat">Sign out</a>
