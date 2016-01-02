7c5e170dd1782f8cdae46eaaaf6793f6037d104a
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-01 20:28:56 -0500

    shifting the header

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,14 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-	<!--<section class="content-header">
-		<h1>Dashboard</h1>-->
-		<ol class="breadcrumb">
-		    <li><a href="?page=dashboard"><i class="fa fa-dashboard"></i> Top</a></li>
-		    <li class="active">Dashboard</li>
-		</ol>
-	<!--</section>-->
+	
+	<ol class="breadcrumb">
+	    <li><a href="?page=dashboard"><i class="fa fa-dashboard"></i> Top</a></li>
+	    <li class="active">Dashboard</li>
+	</ol>
+
+	<section class="content-header">
+		<h1>Dashboard</h1>
+	</section>
 
 	<!-- Main content -->
 	<section class="content">
@@ -15,7 +17,7 @@
 		<div class="col-md-6">
 	<!-- Content Header (Page header) -->
 
-		<h1>Dashboard</h1>
+		<!--<h1>Dashboard</h1>-->
 
 			<!-- Your Page Content Here -->
 			<p>
