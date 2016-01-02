65b4e1cc6d5979019ce75b28ecdb3a17b9706809
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-01 20:25:32 -0500

    Reverted to other breadcrumb

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,12 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-	<section class="content-header">
-		<h1>Dashboard</h1>
+	<!--<section class="content-header">
+		<h1>Dashboard</h1>-->
 		<ol class="breadcrumb">
 		    <li><a href="?page=dashboard"><i class="fa fa-dashboard"></i> Top</a></li>
 		    <li class="active">Dashboard</li>
 		</ol>
-	</section>
+	<!--</section>-->
 
 	<!-- Main content -->
 	<section class="content">
@@ -15,7 +15,7 @@
 		<div class="col-md-6">
 	<!-- Content Header (Page header) -->
 
-		<h2>Dashboard</h2>
+		<h1>Dashboard</h1>
 
 			<!-- Your Page Content Here -->
 			<p>
