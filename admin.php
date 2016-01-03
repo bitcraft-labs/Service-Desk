96234bf3be2aaa21c0411b703d969f585821323c
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-02 21:21:15 -0800

    Add content to admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,22 @@
-<?php
-echo "Admin Panel";
-?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+	
+	<ol class="breadcrumb">
+	    <li><a href="?page=dashboard"><i class="fa fa-dashboard"></i> Top</a></li>
+	    <li class="active">Admin</li>
+	</ol>
+
+	<section class="content-header">
+		<h1>Admin Panel</h1>
+	</section>
+
+	<!-- Main content -->
+	<section class="content">
+	<!--<div class="container-fluid">-->
+	<div class="row">
+		<div class="col-md-6">
+			<p></p>
+		</div>
+	</div>
+	</section><!-- /.content -->
+</div><!-- /.content-wrapper -->
