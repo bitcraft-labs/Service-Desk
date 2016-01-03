93145e962d4c21cdfee17e53effc275f9f159bdd
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 01:22:33 -0800

    Fix Alignment

--- dashboard.php
+++ dashboard.php
@@ -35,11 +35,18 @@
         </div><!-- /.col -->
 		<div class="col-md-3 col-sm-6 col-xs-12">
 			<div class="info-box">
-	            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
-	            <div class="info-box-content">
-	              <span class="info-box-text">Messages</span>
-	              <span class="info-box-number">1,410</span>
-	            </div><!-- /.info-box-content -->
+	        <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+	        <div class="info-box-content">
+	          <span class="info-box-text">Messages</span>
+	          <span class="info-box-number">1,410</span>
+	        </div><!-- /.info-box-content -->
+          </div><!-- /.info-box -->
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Assigned Tickets</span>
+              <span class="info-box-number">8</span>
+            </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
 		</div>
 		<div class="col-md-3 col-sm-6 col-xs-12">
