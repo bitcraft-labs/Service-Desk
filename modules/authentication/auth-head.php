6dc50e3dee8b61b103919abf837f646b1f595ea8
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-02 16:27:48 -0800

    Add iCheck class

--- modules/authentication/auth-head.php
+++ modules/authentication/auth-head.php
@@ -4,12 +4,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-    <link rel="stylesheet" href="dist/css/login.css">
+    <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="/dist/css/login.css">
+    <!-- iCheck -->
+    <link href="/plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
     <!-- Marked for deletion
     <script type="text/javascript" src="modules/authentication/pwdwidget.js"></script>
     -->
-    <script type="text/javascript" src="modules/authentication/gen_validatorv31.js"></script>
-    <script type="text/javascript" src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/modules/authentication/gen_validatorv31.js"></script>
+    <script type="text/javascript" src="/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
 </head>
