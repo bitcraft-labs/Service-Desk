c210b9e37b3016ac314924ed7ba43274a2ee54db
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 01:20:31 -0800

    Add Dashboard Widgets

--- dashboard.php
+++ dashboard.php
@@ -12,25 +12,45 @@
 	<section class="content">
 	<!--<div class="container-fluid">-->
 	<div class="row">
-		<div class="col-md-6">
-	<!-- Content Header (Page header) -->
-
-		<!--<h1>Dashboard</h1>-->
-
-			<!-- Your Page Content Here -->
-			<p>
-			To begin, use the <em>SR Lookup</em> box to the left.<br />
-			<?php
-				if (isset($_GET['sr'])) {
-					$sr = $_GET['sr'];
-					echo "You're looking for Service Record: $sr";
-				} else {} ?>
-			</p>
-		    <p>Write login from scratch. Reusing old code isn't working. Not easily adaptable. get rid of includes; move everything to modules.</p>
-			<p><a href="updates/">Download Desktop Application</a></p>
+		<div class="col-md-6 col-xs-12">
+          <!-- Widget: user widget style 1 -->
+          <div class="box box-widget widget-user-2">
+            <!-- Add the bg color to the header using any of the bg-* classes -->
+            <div class="widget-user-header bg-yellow">
+              <div class="widget-user-image">
+                <img class="img-circle" src="../dist/img/user7-128x128.jpg" alt="User Avatar">
+              </div><!-- /.widget-user-image -->
+              <h3 class="widget-user-username">Nadia Carmichael</h3>
+              <h5 class="widget-user-desc">Lead Developer</h5>
+            </div>
+            <div class="box-footer no-padding">
+              <ul class="nav nav-stacked">
+                <li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
+                <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
+                <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
+                <li><a href="#">Followers <span class="pull-right badge bg-red">842</span></a></li>
+              </ul>
+            </div>
+          </div><!-- /.widget-user -->
+        </div><!-- /.col -->
+		<div class="col-md-3 col-sm-6 col-xs-12">
+			<div class="info-box">
+	            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
+	            <div class="info-box-content">
+	              <span class="info-box-text">Messages</span>
+	              <span class="info-box-number">1,410</span>
+	            </div><!-- /.info-box-content -->
+          </div><!-- /.info-box -->
 		</div>
-		<div class="col-md-6">
-		</div>
+		<div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
+            <div class="info-box-content">
+              <span class="info-box-text">Assigned Tickets</span>
+              <span class="info-box-number">8</span>
+            </div><!-- /.info-box-content -->
+          </div><!-- /.info-box -->
+        </div><!-- /.col -->
 	</div>
 	</section><!-- /.content -->
 </div><!-- /.content-wrapper -->
