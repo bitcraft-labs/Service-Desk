7b79b644960d49cf36f95fdc7187d435f4cba2d3
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 01:34:59 -0800

    Add two widgets

--- dashboard.php
+++ dashboard.php
@@ -32,7 +32,7 @@
             </div>
           </div><!-- /.widget-user -->
         </div><!-- /.col -->
-		<div class="col-sm-6 col-xs-12">
+		<div class="col-md-6 col-xs-12">
 			<div class="info-box">
 	        <span class="info-box-icon bg-yellow"><i class="fa fa-envelope-o"></i></span>
 	        <div class="info-box-content">
@@ -47,8 +47,119 @@
               <span class="info-box-number">31</span>
             </div><!-- /.info-box-content -->
           </div><!-- /.info-box -->
+		</div>	
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-xs-12">
+			<div class="box">
+				<div class="box-header">
+				  <h3 class="box-title">Recent Activity</h3>
+				</div><!-- /.box-header -->
+				<div class="box-body">
+					<table id="staff_list" class="table table-bordered table-striped">
+						<thead>
+						<?php $tabhead="
+							<tr>
+								<td>#</td>
+								<td>First Name</td>
+								<td>Last Name</td>
+								<td>User Group</td>
+								<td>Active SR #</td>
+								<td>Last Online</td>
+							</tr>"; echo $tabhead; ?>
+						</thead>
+						<tbody>
+							<?php /*
+							$dal = new DAL();
+							$personinfo = $dal->getPersonInfo('all');
+							if ($personinfo) {
+								foreach($personinfo as $row) {
+							    echo "<tr>
+								    	<td><a href='?id=$row->user_id'>$row->user_id</a></td>
+								    	<td>$row->name</td>
+								    	<td>@$row->banner_id</td>
+								    	<td>$row->user_type</td>
+								    	<td>$row->phone</td>
+								    	<td>$row->email</td>
+								    	<td>12/1/2015</td>
+								    	</tr>";
+							  	}
+							} */
+							?>
+							<!--
+							<tr>
+								<td><a href="?id=1">1</a></td>
+								<td>Jo Shmo</td>
+								<td>@01234567</td>
+								<td>Student</td>
+								<td>555.555.5555</td>
+								<td>jshmo1@student.fitchburgstate.edu</td>
+								<td>12/3/2015</td>
+							</tr>
+							-->
+						</tbody>
+						<tfoot>
+							<?php echo $tabhead; ?>
+						</tfoot>
+					</table>
+				</div>
+			</div>
 		</div>
-		
+		<div class="col-md-6 col-xs-12">
+			<div class="box">
+				<div class="box-header">
+				  <h3 class="box-title">Active Staff Users</h3>
+				</div><!-- /.box-header -->
+				<div class="box-body">
+					<table id="staff_list" class="table table-bordered table-striped">
+						<thead>
+						<?php $tabhead="
+							<tr>
+								<td>#</td>
+								<td>First Name</td>
+								<td>Last Name</td>
+								<td>User Group</td>
+								<td>Active SR #</td>
+								<td>Last Online</td>
+							</tr>"; echo $tabhead; ?>
+						</thead>
+						<tbody>
+							<?php /*
+							$dal = new DAL();
+							$personinfo = $dal->getPersonInfo('all');
+							if ($personinfo) {
+								foreach($personinfo as $row) {
+							    echo "<tr>
+								    	<td><a href='?id=$row->user_id'>$row->user_id</a></td>
+								    	<td>$row->name</td>
+								    	<td>@$row->banner_id</td>
+								    	<td>$row->user_type</td>
+								    	<td>$row->phone</td>
+								    	<td>$row->email</td>
+								    	<td>12/1/2015</td>
+								    	</tr>";
+							  	}
+							} */
+							?>
+							<!--
+							<tr>
+								<td><a href="?id=1">1</a></td>
+								<td>Jo Shmo</td>
+								<td>@01234567</td>
+								<td>Student</td>
+								<td>555.555.5555</td>
+								<td>jshmo1@student.fitchburgstate.edu</td>
+								<td>12/3/2015</td>
+							</tr>
+							-->
+						</tbody>
+						<tfoot>
+							<?php echo $tabhead; ?>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</div>
 	</div>
 	</section><!-- /.content -->
 </div><!-- /.content-wrapper -->
