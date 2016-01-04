8b512e2f45d0811a7652110638a6e23825761298
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-04 01:29:42 -0500

    Output user type

--- index2.php
+++ index2.php
@@ -1,4 +1,5 @@
 <?php
+	echo "User Type: ".$_SESSION['user_type'];
   if ($_SESSION['user_type'] == 3) {
     echo "<script type='text/javascript'>
       window.location.replace('EndUserPortal.php');
