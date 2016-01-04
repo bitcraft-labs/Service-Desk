39d0fec0d653cf12e698937381ee86b4f9299e53
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-04 01:25:42 -0500

    Attempt with JS

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ Status:     Staging; Idea Testing; Development
 -->
 <?php
   if ($_SESSION['user_type'] == 3) {
-    header('Location: ./EndUserPortal.php');
+    echo "<script type='text/javascript'>
+      window.location.replace('EndUserPortal.php');
+      </script>";
     exit;
   }
 ?>
