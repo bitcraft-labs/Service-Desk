8e192442fcca5dbdd277053e632159fc59bc6b5d
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 19:43:59 -0500

    Create End User Portal

---
+++ EndUserPortal.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<!--
+Project:    Honey Badger (PHB) - Working Title
+Lead Devs:  Joshua Nasiatka & Allen Perry
+For:        Software Engineering
+Dev Date:   Spring 2016
+Status:     Staging; Idea Testing; Development
+-->
+<html>
+<?php 
+include_once 'modules/config.inc.php'; 
+require_once("./modules/authentication/config.php");
+
+if(!$authenticator->CheckLogin())
+{
+    $authenticator->RedirectToURL("login.php");
+    exit;
+}
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php echo $title; ?></title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="plugins/datatables/dataTables.bootstrap.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+          page. However, you can choose any other skin. Make sure you
+          apply the skin class to the body tag so the changes take effect.
+    -->
+    <?php echo "<link rel='stylesheet' href='dist/css/skins/skin-$skin.min.css'>"; ?>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+  <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+  <?php echo "<body class='hold-transition skin-$skin layout-top-nav'>"; ?>
+    <div class="wrapper">
+
+      <?php include_once 'modules/end_user_portal/header.php'; ?>
+      <!-- Full Width Column -->
+      <div class="content-wrapper">
+        <div class="container">
+          <!-- Content Header (Page header) -->
+          <section class="content-header">
+            <h1>
+              Top Navigation
+              <small>Example 2.0</small>
+            </h1>
+            <ol class="breadcrumb">
+              <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+              <li><a href="#">Layout</a></li>
+              <li class="active">Top Navigation</li>
+            </ol>
+          </section>
+
+          <!-- Main content -->
+          <section class="content">
+            <div class="callout callout-info">
+              <h4>Tip!</h4>
+              <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular links instead.</p>
+            </div>
+            <div class="callout callout-danger">
+              <h4>Warning!</h4>
+              <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar and the content will slightly differ than that of the normal layout.</p>
+            </div>
+            <div class="box box-default">
+              <div class="box-header with-border">
+                <h3 class="box-title">Blank Box</h3>
+              </div>
+              <div class="box-body">
+                The great content goes here
+              </div><!-- /.box-body -->
+            </div><!-- /.box -->
+          </section><!-- /.content -->
+        </div><!-- /.container -->
+      </div><!-- /.content-wrapper -->
+      <?php	include_once 'modules/footer.php'; ?>
+    </div><!-- ./wrapper -->
+
+    <!-- jQuery 2.1.4 -->
+    <script src="../../plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../../bootstrap/js/bootstrap.min.js"></script>
+    <!-- SlimScroll -->
+    <script src="../../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="../../plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/app.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../dist/js/demo.js"></script>
+  </body>
+</html>
