920a79763873d3f8c4d5dc7b99450d6bbfdfc168
Joshua Nasiatka <joshuanasiatka@users.noreply.github.com>
2016-01-04 10:35:51 -0500

    Middle-man redirect script

--- index.php
+++ index.php
@@ -6,80 +6,26 @@ For:        Software Engineering
 Dev Date:   Spring 2016
 Status:     Staging; Idea Testing; Development
 -->
+
+<html>
+<!-- head.php contains the stylesheets -->
+<?php 
+include_once 'modules/head.php'; ?>
+<body>
 <?php
   if ($_SESSION['user_type'] == 3) {
+  	echo "If page is stuck, click <a href='EndUserPortal.php'>here</a> to continue.";
     echo "<script type='text/javascript'>
       window.location.replace('EndUserPortal.php');
       </script>";
     exit;
+  } else {
+  	echo "If page is stuck, click <a href='HelpDesk.php'>here</a> to continue.";
+  	echo "<script type='text/javascript'>
+      window.location.replace('HelpDesk.php');
+      </script>";
+    exit;
   }
 ?>
-<html>
-	<!-- head.php contains the stylesheets -->
-  	<?php 
-    include_once 'modules/head.php'; ?>
-    <?php echo "<body class='hold-transition skin-$skin sidebar-mini'>"; ?>
-    <div class="wrapper">
-		<?php 
-		// build the user interface
-		include_once 'modules/header.php';
-		include_once 'modules/left_sidebar.php';
-		include_once 'modules/page_handling.php';
-		include_once 'modules/footer.php'; 
-		include_once 'modules/control_sidebar.php';?>
-    </div><!-- ./wrapper -->
-
-<!-- REQUIRED JS SCRIPTS -->
-<!-- jQuery 2.1.4 -->
-<script src="/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-<!-- Bootstrap 3.3.5 -->
-<script src="/bootstrap/js/bootstrap.min.js"></script>
-<!-- AdminLTE App -->
-<script src="/dist/js/app.min.js"></script>
-<!-- DataTables -->
-<script src="plugins/datatables/jquery.dataTables.js"></script>
-<script src="plugins/datatables/dataTables.bootstrap.js"></script>
-<!-- SlimScroll -->
-<script src="plugins/slimScroll/jquery.slimscroll.js"></script>
-<!-- FastClick -->
-<script src="plugins/fastclick/fastclick.min.js"></script>
-<!-- InputMask -->
-<script src="plugins/input-mask/jquery.inputmask.js"></script>
-<script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
-<script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
-
-<!-- page script -->
-<script>
-  $(function () {
-    $('#active_staff').DataTable({
-      "paging": false,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": false,
-      "info": true,
-      "autoWidth": false
-    });
-  });
-</script>
-
-<script>
-  $(function () {
-    $('#recent_activity').DataTable({
-      "paging": false,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": false,
-      "info": true,
-      "autoWidth": false
-    });
-  });
-</script>
-
-    <!-- Optionally, you can add Slimscroll and FastClick plugins.
-         Both of these plugins are recommended to enhance the
-         user experience. Slimscroll is required when using the
-         fixed layout. -->
-    <?php
-    include_once 'modules/modals.php'; ?>
-  </body>
+</body>
 </html>
