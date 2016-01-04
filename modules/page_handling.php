444b9b29eb0a161f283cdb396a51c53e7ab3a188
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-04 01:12:29 -0500

    New redirection script

--- modules/page_handling.php
+++ modules/page_handling.php
@@ -7,7 +7,7 @@ if (isset($_GET['page'])) {
 		$allowed_sector = $allowed_pages;
 	} else {
 		$allowed_sector = array('');
-		header('Location: /EndUserPortal.php');
+		echo "<script>window.location.replace('/EndUserPortal.php');</script>";
 	}
 	if (!in_array($requested_page, $allowed_sector)) {
 		include_once '404.php';
