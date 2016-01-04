5accb6fa1501dbe887b1dbf7c1e9393cbdcfa3ea
Joshua Nasiatka <joshuanasiatka@users.noreply.github.com>
2016-01-04 10:39:42 -0500

    Delete index2.php
    No longer necessary

--- index2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	echo "User Type: ".$_SESSION['user_type'];
-  if ($_SESSION['user_type'] == 3) {
-    echo "<script type='text/javascript'>
-      window.location.replace('EndUserPortal.php');
-      </script>";
-    exit;
-  }
-?>
