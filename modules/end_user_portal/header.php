26d00b6a72390d1e82fe13d52d5b73d808fe3c0d
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 23:02:43 -0500

    Enhancement

--- modules/end_user_portal/header.php
+++ modules/end_user_portal/header.php
@@ -2,7 +2,7 @@
 	<nav class="navbar navbar-static-top">
 	  <div class="container">
 	    <div class="navbar-header">
-	      <a href="./" class="navbar-brand"><span style="padding-top:-10px;"><?php echo "$formatted_logo_top"?></a></span>
+	      <a href="./" class="navbar-brand"><?php echo "$formatted_logo_top"?></a>
 	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
 	        <i class="fa fa-bars"></i>
 	      </button>
@@ -132,14 +132,14 @@
 	              <!-- The user image in the navbar-->
 	              <img src="../../dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
 	              <!-- hidden-xs hides the username on small devices so only the image appears. -->
-	              <span class="hidden-xs">Alexander Pierce</span>
+	              <span class="hidden-xs"><?= $authenticator->UserFullName() ?></span>
 	            </a>
 	            <ul class="dropdown-menu">
 	              <!-- The user image in the menu -->
 	              <li class="user-header">
 	                <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 	                <p>
-	                  Alexander Pierce - Web Developer
+	                  <?= $authenticator->UserFullName() ?>
 	                  <small>Member since Nov. 2012</small>
 	                </p>
 	              </li>
