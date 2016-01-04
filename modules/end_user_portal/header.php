1b08537afdc9a4965d3ac418d87da77aabfaf7ea
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 22:06:15 -0500

    Revert

--- modules/end_user_portal/header.php
+++ modules/end_user_portal/header.php
@@ -2,7 +2,6 @@
 	<nav class="navbar navbar-static-top">
 	  <div class="container">
 	    <div class="navbar-header">
-    	  <a href="./" class="navbar-brand"><b>Admin</b>LTE</a>
 	      <a href="./" class="navbar-brand"><?php echo "$formatted_logo_top"?></a>
 	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
 	        <i class="fa fa-bars"></i>
