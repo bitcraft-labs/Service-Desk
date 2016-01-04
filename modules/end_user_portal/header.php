47c86345af1cae2d8608068923c3bd8ed184d0f3
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-03 22:09:28 -0500

    Shift logo up

--- modules/end_user_portal/header.php
+++ modules/end_user_portal/header.php
@@ -2,7 +2,7 @@
 	<nav class="navbar navbar-static-top">
 	  <div class="container">
 	    <div class="navbar-header">
-	      <a href="./" class="navbar-brand"><?php echo "$formatted_logo_top"?></a>
+	      <a href="./" class="navbar-brand"><span style="padding-top:-10px;"><?php echo "$formatted_logo_top"?></a></span>
 	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
 	        <i class="fa fa-bars"></i>
 	      </button>
