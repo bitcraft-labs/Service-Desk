0344589e58495afa8af4948e22df573496d75955
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-04 01:42:42 -0500

    Add avatar image

--- modules/end_user_portal/header.php
+++ modules/end_user_portal/header.php
@@ -130,14 +130,14 @@
 	            <!-- Menu Toggle Button -->
 	            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
 	              <!-- The user image in the navbar-->
-	              <img src="/dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+	              <img src="/dist/img/avatar2.jpg" class="user-image" alt="User Image">
 	              <!-- hidden-xs hides the username on small devices so only the image appears. -->
 	              <span class="hidden-xs"><?= $authenticator->UserFullName() ?></span>
 	            </a>
 	            <ul class="dropdown-menu">
 	              <!-- The user image in the menu -->
 	              <li class="user-header">
-	                <img src="/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+	                <img src="/dist/img/avatar2.jpg" class="img-circle" alt="User Image">
 	                <p>
 	                  <?= $authenticator->UserFullName() ?>
 	                  <small>Member since Nov. 2012</small>
