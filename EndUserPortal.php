2a26043594fc0659f851839ddd87cdb9d528ee79
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-04 01:48:08 -0500

    Update wording

--- EndUserPortal.php
+++ EndUserPortal.php
@@ -57,32 +57,31 @@ if(!$authenticator->CheckLogin())
           <!-- Content Header (Page header) -->
           <section class="content-header">
             <h1>
-              Top Navigation
-              <small>Example 2.0</small>
+              End User Portal
+              <small>Tronic CRM <?= $app_version?></small>
             </h1>
             <ol class="breadcrumb">
               <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-              <li><a href="#">Layout</a></li>
-              <li class="active">Top Navigation</li>
+              <li class="active">Dashboard</li>
             </ol>
           </section>
 
           <!-- Main content -->
           <section class="content">
-            <div class="callout callout-info">
+            <!--<div class="callout callout-info">
               <h4>Tip!</h4>
               <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular links instead.</p>
             </div>
             <div class="callout callout-danger">
               <h4>Warning!</h4>
               <p>The construction of this layout differs from the normal one. In other words, the HTML markup of the navbar and the content will slightly differ than that of the normal layout.</p>
-            </div>
+            </div>-->
             <div class="box box-default">
               <div class="box-header with-border">
-                <h3 class="box-title">Blank Box</h3>
+                <h3 class="box-title">The End User Gateway Portal</h3>
               </div>
               <div class="box-body">
-                The great content goes here
+                Welcome back! Feel free to take a look at your previous computer checkups, webmail messages, or what have you.<br />Have a wonderful day!
               </div><!-- /.box-body -->
             </div><!-- /.box -->
           </section><!-- /.content -->
