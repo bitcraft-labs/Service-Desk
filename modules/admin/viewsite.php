e80fefa30fcf6f3c4efe81a0b8bd0feedad9b3db
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-08 00:09:02 -0500

    Show Table

--- modules/admin/viewsite.php
+++ modules/admin/viewsite.php
@@ -4,5 +4,107 @@
 				 echo $adminsub; 
 				 $adminhsub = '<small>'.$adminsub.'</small>';
 		?></p>
+		<form class="form-horizontal">
+			<!-- Form Name -->
+			<legend>Site Settings</legend>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="db_host">Database Host</label>  
+			  <div class="col-md-4">
+			  <input id="db_host" name="db_host" type="text" placeholder="db_host" class="form-control input-md" required="">
+			    
+			  </div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="db_user">Database User</label>  
+			  <div class="col-md-4">
+			  <input id="db_user" name="db_user" type="text" placeholder="db_user" class="form-control input-md" required="">
+			    
+			  </div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="db_pass">Database Pass</label>  
+			  <div class="col-md-4">
+			  <input id="db_pass" name="db_pass" type="text" placeholder="db_pass" class="form-control input-md" required="">
+			    
+			  </div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="db_name">Database Name</label>  
+			  <div class="col-md-4">
+			  <input id="db_name" name="db_name" type="text" placeholder="db_name" class="form-control input-md" required="">
+			    
+			  </div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="company_name">Company Name</label>  
+			  <div class="col-md-4">
+			  <input id="company_name" name="company_name" type="text" placeholder="Bitcraft Labs" class="form-control input-md">
+			    
+			  </div>
+			</div>
+
+			<!-- Textarea -->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="formatted_company_name">HTML Formatted Co. Name</label>
+			  <div class="col-md-4">                     
+			    <textarea class="form-control" id="formatted_company_name" name="formatted_company_name">&lt;strong&gt;BITCRAFT&lt;/strong&gt; LABS&lt;br /&gt;Technology Repair Database</textarea>
+			  </div>
+			</div>
+
+			<!-- Text input-->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="sysemail">Security Notifications Email</label>  
+			  <div class="col-md-4">
+			  <input id="sysemail" name="sysemail" type="text" placeholder="security@domain.com" class="form-control input-md" required="">
+			    
+			  </div>
+			</div>
+
+			<!-- Select Basic -->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="skin">Select Theme</label>
+			  <div class="col-md-4">
+			    <select id="skin" name="skin" class="form-control">
+			      <option value="green">green</option>
+			      <option value="yellow">yellow</option>
+			      <option value="blue">blue</option>
+			    </select>
+			  </div>
+			</div>
+
+			<!-- File Button --> 
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="main_logo">Upload Main Logo</label>
+			  <div class="col-md-4">
+			    <input id="main_logo" name="main_logo" class="input-file" type="file">
+			  </div>
+			</div>
+
+			<!-- File Button --> 
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="main_logo_top">Upload Top Bar Logo</label>
+			  <div class="col-md-4">
+			    <input id="main_logo_top" name="main_logo_top" class="input-file" type="file">
+			  </div>
+			</div>
+
+			<!-- Button -->
+			<div class="form-group">
+			  <label class="col-md-4 control-label" for="submit"></label>
+			  <div class="col-md-4">
+			    <button id="submit" name="submit" class="btn btn-primary">Update</button>
+			  </div>
+			</div>
+		</form>
 	</div>
 </div>
