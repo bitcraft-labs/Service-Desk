ce169879706b375ce81d7dabb2ed9156f428cc75
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-08 00:32:42 -0500

    Referenced config in wrong location

--- modules/admin/head.php
+++ modules/admin/head.php
@@ -2,7 +2,7 @@
 include_once 'modules/config.inc.php'; 
 //include_once 'modules/login_auth/_validate.php';
 require_once("./modules/authentication/config.php");
-require_once("./modules/authentication/config2.php");
+include_once "./modules/admin/config2.php";
 
 if(!$authenticator->CheckLogin())
 {
