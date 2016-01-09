8bd0b17c42fa682c69e330d96b59b7af74fdd7b5
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-08 22:04:22 -0500

    Empty the branch

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bitcraft Service Desk
-### Cloud-based IT Help Desk Management Software
-Based on AdminLTE
-
-Bitcraft Service Desk aims to eliminate paperwork in computer repairs.
--- modules/end_user_portal/footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-	<div class="container">
-	  <div class="pull-right hidden-xs">
-	    <strong>Version</strong> <?php echo $app_version; ?></a>
-	  </div>
-	  <strong>Copyright &copy; 2015 <a href="https://bitcraftlabs.net/">Bitcraft Labs</a>.</strong> All rights reserved.
-	</div><!-- /.container -->
-</footer>
