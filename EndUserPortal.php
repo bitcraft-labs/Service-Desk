fe7ac1c5a3657736e11caad816ae38cf58a3788e
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-10 03:03:47 -0800

    Fix typo with page generation

--- EndUserPortal.php
+++ EndUserPortal.php
@@ -52,7 +52,7 @@ if(!$authenticator->CheckLogin())
 
       <?php
       include_once 'modules/end_user_portal/header.php';
-      if ($page='mailbox') {
+      if ($_GET['page'] == 'mailbox') {
         include_once 'modules/mailbox/mailbox-ui.php';
       } else { ?>
       <!-- Full Width Column -->
