eace64cb4021990b76fab9c70b35f6b726313b54
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-11 19:00:41 -0500

    Created Profile.php
    Moved ?page=profile to profile-ui.php in modules

--- profile.php
+++ profile.php
@@ -1,22 +1,34 @@
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-	<ol class="breadcrumb">
-	    <li><a href="?page=dashboard"><i class="fa fa-dashboard"></i> Top</a></li>
-	    <li class="active">Profile</li>
-	</ol>
+<!DOCTYPE html>
+<!--
+Project:    Bitcraft Service Desk (Working Title)
+Lead Devs:  Joshua Nasiatka, Allen Perry, Eugene Duffy
+For:        Software Engineering
+Dev Date:   Spring 2016
+Status:     Staging; Idea Testing; Development
+-->
+<html>
+  	<?php
+    $page_title = 'Profile';
+    include_once 'modules/head.php'; ?>
+    <?php echo "<body class='hold-transition skin-$skin sidebar-mini'>"; ?>
+    <div class="wrapper">
+		<?php
+		// build the user interface
+		include_once 'modules/header.php';
+		include_once 'modules/left_sidebar.php';
+    include_once 'modules/profile/profile-ui.php';
+    include_once 'modules/footer.php';
+		include_once 'modules/control_sidebar.php';
+    ?>
+    </div><!-- ./wrapper -->
 
-	<!-- Content Header (Page header) -->
-	<section class="content-header">
-		<h1>Profile</h1>
-		<h3><?= $authenticator->UserFullName() ?></h3>
-	</section>
+    <!-- REQUIRED JS SCRIPTS -->
+    <!-- jQuery 2.1.4 -->
+    <script src="/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="/dist/js/app.min.js"></script>
 
-	<!-- Main content -->
-	<section class="content">
-		<!-- Your Page Content Here -->
-		<p>Full Name: <?= $authenticator->UserFullName() ?><br />
-		Username: <?php echo $_SESSION['usr_0bade6db4c']; ?><br />
-		Primary Email: <?= $authenticator->UserEmail()  ?></p>
-		<p><a href="change-pass.php">Change Password</a></p>
-	</section><!-- /.content -->
-</div><!-- /.content-wrapper -->
+  </body>
+</html>
