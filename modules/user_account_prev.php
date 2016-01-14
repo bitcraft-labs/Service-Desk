32b568066fd503a4a8943fe192fe4a7ce198e79a
Joshua Nasiatka <joshuanasiatka@users.noreply.github.com>
2016-01-13 22:35:52 -0500

    Update user_account_prev.php

--- modules/user_account_prev.php
+++ modules/user_account_prev.php
@@ -41,7 +41,7 @@
     <!-- Menu Footer-->
     <li class="user-footer">
       <div class="pull-left">
-        <a href="profile.php" class="btn btn-default btn-flat">Profile</a>
+        <a href="Profile.php" class="btn btn-default btn-flat">Profile</a>
       </div>
       <div class="pull-right">
         <a href="logout.php" class="btn btn-default btn-flat">Sign out</a>
