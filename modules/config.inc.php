fc2b9588c7fd852193c44933fc91fac4fba2075b
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-24 21:59:22 -0500

    Update file for new variable style

--- modules/config.inc.php
+++ modules/config.inc.php
@@ -56,34 +56,33 @@ if (file_exists('custom/config.php')) {
 	$custom = 'custom';
 	$cusimg = 'custom/img';
 
-	if ($company_name) {
-		$coname = $company_name;
-	}
-
-	if ($formatted_company_name) {
-		$formatted_coname = $formatted_company_name;
-	}
+	$skin 				= $conf['customize']['skin'];
+	$coname 			= $conf['site']['company_name'];
+	$formatted_coname 	= $conf['site']['formatted_company_name'];
+	$main_logo 			= $cusimg.'/'.$conf['customize']['main_logo'];
+	$main_logo_small 	= $cusimg.'/'.$conf['customize']['main_logo_small'];
+	$main_logo_top 		= $cusimg.'/'.$conf['customize']['main_logo_top'];
 
 	if ($main_logo) {
-		if (file_exists("$cusimg/$main_logo")) {
-			$logo = "$cusimg/$main_logo";             //main logo
+		if (file_exists($main_logo)) {
+			$logo = $main_logo;             //main logo
 			$loginHeader = "<p style='padding-top:15px;'><img src='$logo' width='100' /></p>";
 		    $loginHeader .= "<h1>$formatted_coname<br /><small>Login</small></h1>";
 		}
 	}
 
 	if ($main_logo_small) {
-		$logo_dark = "$cusimg/$main_logo_small";  //sidebar and mobile logo
+		$logo_dark = $main_logo_small;  //sidebar and mobile logo
 	}
 
 	if ($main_logo_top) {
-		$logo_top = "$cusimg/$main_logo_top";     //logo on top navigation bar
+		$logo_top = $main_logo_top;     //logo on top navigation bar
 		$formatted_logo_top = "<img src='$logo_top' width='155' />";
 	}
 }
 
 /* ------------- APP NAME, VERSION NUMBER, ETC. ------------ */
-$app_name = "Technology Repair Database";
+$app_name = "Bitcraft Service Desk";
 $app_version = "0.0.1";
 
 /* Sets the title in the header. This is what the page will be named by default when you
