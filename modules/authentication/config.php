78078f8e5131763220a061816f6ce65cb452048d
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-24 22:17:54 -0500

    Update authenticator config for new variable style

--- modules/authentication/config.php
+++ modules/authentication/config.php
@@ -8,16 +8,16 @@ $authenticator = new Authenticator();
 $authenticator->SetWebsiteName('bitcraftlabs.net');
 
 //Provide the email address where you want to get notifications
-$authenticator->SetAdminEmail('joshuanasiatka@gmail.com');
+$authenticator->SetAdminEmail($conf['customize']['sysemail']);
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$authenticator->InitDB(/*hostname*/   $db_hostname,
-					  /*username*/   $db_username,
-					  /*password*/   $db_password,
-					  /*database*/   $db_name,
+$authenticator->InitDB(/*hostname*/  $conf['sql']['host'], //$db_hostname,
+					  /*username*/   $conf['sql']['user'], //$db_username,
+					  /*password*/   $conf['sql']['pass'], //$db_password,
+					  /*database*/   $conf['sql']['name'], //$db_name,
 					  /*table name*/ 'users');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
