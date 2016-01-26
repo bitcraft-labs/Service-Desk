9132ac34276d1a4134a9fdbbc2118d9a7ecc96d6
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-01-26 00:34:56 -0500

    Revert "Remove hard code for variable"
    This reverts commit d3e275b99318bc77113fbb607b7672b1dfbb9c98.

--- modules/admin/viewsite.php
+++ modules/admin/viewsite.php
@@ -1,70 +1,70 @@
 <div class="row">
 	<div class="col-md-12">
-		<p><?php $adminsub = 'View Site Settings';
-				 echo $adminsub;
+		<p><?php $adminsub = 'View Site Settings'; 
+				 echo $adminsub; 
 				 $adminhsub = '<small>'.$adminsub.'</small>';
 		?></p>
 		<form class="form-horizontal">
 
 			<!-- Text input-->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="db_host">Database Host</label>
+			  <label class="col-md-4 control-label" for="db_host">Database Host</label>  
 			  <div class="col-md-4">
 			  <?php echo "<input id='db_host' name='db_host' type='text' placeholder='db_host' class='form-control input-md' value='".$conf['sql']['host']."' required=''>";?>
-
+			    
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="db_user">Database User</label>
+			  <label class="col-md-4 control-label" for="db_user">Database User</label>  
 			  <div class="col-md-4">
 			  <?php echo "<input id='db_user' name='db_user' type='text' placeholder='db_user' class='form-control input-md' value='".$conf['sql']['user']."' required=''>";?>
-
+			    
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="db_pass">Database Pass</label>
+			  <label class="col-md-4 control-label" for="db_pass">Database Pass</label>  
 			  <div class="col-md-4">
 			  <?php echo "<input id='db_pass' name='db_pass' type='text' placeholder='db_pass' class='form-control input-md' value='".$conf['sql']['pass']."' required=''>";?>
-
+			    
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="db_name">Database Name</label>
+			  <label class="col-md-4 control-label" for="db_name">Database Name</label>  
 			  <div class="col-md-4">
 			  <?php echo "<input id='db_name' name='db_name' type='text' placeholder='db_name' class='form-control input-md' value='".$conf['sql']['name']."' required=''>";?>
-
+			    
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="company_name">Company Name</label>
+			  <label class="col-md-4 control-label" for="company_name">Company Name</label>  
 			  <div class="col-md-4">
 			  <?php echo "<input id='company_name' name='company_name' type='text' placeholder='Bitcraft Labs' value='".$conf['site']['company_name']."' class='form-control input-md'>";?>
-
+			    
 			  </div>
 			</div>
 
 			<!-- Textarea -->
 			<div class="form-group">
 			  <label class="col-md-4 control-label" for="formatted_company_name">HTML Formatted Co. Name</label>
-			  <div class="col-md-4">
-			    <textarea class="form-control" id="formatted_company_name" name="formatted_company_name"><?= htmlspecialchars($conf['site']['formatted_company_name']) ?></textarea>
+			  <div class="col-md-4">                     
+			    <textarea class="form-control" id="formatted_company_name" name="formatted_company_name">&lt;strong&gt;BITCRAFT&lt;/strong&gt; LABS&lt;br /&gt;Technology Repair Database</textarea>
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="form-group">
-			  <label class="col-md-4 control-label" for="sysemail">Security Notifications Email</label>
+			  <label class="col-md-4 control-label" for="sysemail">Security Notifications Email</label>  
 			  <div class="col-md-4">
 			  <?php echo "<input id='sysemail' name='sysemail' type='text' placeholder='security@domain.com' class='form-control input-md' value='".$conf['customize']['sysemail']."' required=''>";?>
-
+			    
 			  </div>
 			</div>
 
@@ -81,7 +81,7 @@
 			  </div>
 			</div>
 
-			<!-- File Button -->
+			<!-- File Button --> 
 			<div class="form-group">
 			  <label class="col-md-4 control-label" for="main_logo">Upload Main Logo</label>
 			  <div class="col-md-4">
@@ -89,7 +89,7 @@
 			  </div>
 			</div>
 
-			<!-- File Button -->
+			<!-- File Button --> 
 			<div class="form-group">
 			  <label class="col-md-4 control-label" for="main_logo_top">Upload Top Bar Logo</label>
 			  <div class="col-md-4">
