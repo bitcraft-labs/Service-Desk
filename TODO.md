ba919d442e329f7c8b3200e261800b5dca8da7df
D3Exclusive <eduffy2@student.fitchburgstate.edu>
2016-02-04 20:28:17 -0500

    Added Custom error pages and error hangling

---
+++ TODO.md
@@ -0,0 +1,10 @@
+Figure out authentication error on login page. -> Figured out now style
+Help Allen do communication section
+Make end user mobile capable
+Do errors page -> Bootstrap
+Modify Footer to pull from one source
+Make table for requests 
+Picture Upload -> All Users 
+Scrap Tasks in navbar
+Add Sign out button like in admin
+Search bar on enter
--- modules/end_user_portal/footer.php
+++ modules/end_user_portal/footer.php
@@ -4,6 +4,6 @@
 	  <div class="pull-right hidden-xs">
 	    <strong>Version</strong> <?php echo $app_version; ?></a>
 	  </div>
-	  <strong>Copyright &copy; 2015 <a href="https://bitcraftlabs.net/">Bitcraft Labs</a>.</strong> All rights reserved.
+	  <strong>Copyright &copy; 2016 <a href="https://bitcraftlabs.net/">Bitcraft Labs</a>.</strong> All rights reserved.
 	</div><!-- /.container -->
 </footer>
