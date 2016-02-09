b8aaa45b1cc3ea5abb4f17e9ec7d1770212f6df6
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-02-08 22:33:58 -0500

    Potentially fix tab glitch

--- Admin.php
+++ Admin.php
@@ -66,6 +66,25 @@ Status:     Staging; Idea Testing; Development
         window.document.location = $(this).data("href");
     });
   });
+
+  $('#admin_tabs').on('click', 'a[data-toggle="tab"]', function(e) {
+      e.preventDefault();
+
+      var $link = $(this);
+
+      if (!$link.parent().hasClass('active')) {
+
+        //remove active class from other tab-panes
+        $('.tab-content:not(.' + $link.attr('href').replace('#','') + ') .tab-pane').removeClass('active');
+
+        // click first submenu tab for active section
+        $('a[href="' + $link.attr('href') + '_all"][data-toggle="tab"]').click();
+
+        // activate tab-pane for active section
+        $('.tab-content.' + $link.attr('href').replace('#','') + ' .tab-pane:first').addClass('active');
+      }
+
+    });
 </script>
 
     <!-- Optionally, you can add Slimscroll and FastClick plugins.
