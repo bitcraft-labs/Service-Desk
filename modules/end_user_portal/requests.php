b7b1b08b6a0cdc3e840ab2052caa2496b304d52a
D3Exclusive <eduffy2@student.fitchburgstate.edu>
2016-02-11 14:03:06 -0500

    Corrected Mobile responsiveness more

--- modules/end_user_portal/requests.php
+++ modules/end_user_portal/requests.php
@@ -16,43 +16,43 @@
 			    	<table id="records" class="table table-bordered table-striped">
 			    		<thead>
 			    			<tr>
-			    				<?php $tabhead = "
+			    				<?php $tabhead = '
 				    				<td>SR#</td>
-				    				<td>Category</td>
+				    				<td class="mobile-table">Category</td>
 				    				<td>Status</td>
 				    				<td>Ticket Title</td>
-				    				<td>Assigned Admin</td>
-				    				<td>Date Checked In</td>
-				    				<td>Date Last Updated</td>";
+				    				<td class="mobile-table">Assigned Admin</td>
+				    				<td class="mobile-table">Date Checked In</td>
+				    				<td>Date Last Updated</td>';
 			    				 echo $tabhead; ?>
 			    			</tr>
 			    		</thead>
 			    		<tbody>
 			    			<tr>
 			    				<td><a href="?page=ViewRequests&sr=1">1</a></td>
-			    				<td>Hardware</td>
+			    				<td class="mobile-table">Hardware</td>
 			    				<td>In Progress</td>
 			    				<td>TT</td>
-			    				<td>helpdesktech</td>
-			    				<td>12/1/2015</td>
+			    				<td class="mobile-table">helpdesktech</td>
+			    				<td class="mobile-table">12/1/2015</td>
 			    				<td>12/2/2015</td>
 			    			</tr>
 			    			<tr>
 			    				<td><a href="?page=ViewRequests&sr=2">2</a></td>
-			    				<td>Software</td>
+			    				<td class="mobile-table">Software</td>
 			    				<td>Waiting for Pickup</td>
 			    				<td>TT</td>
-			    				<td>helpdesktech</td>
-			    				<td>11/30/2015</td>
+			    				<td class="mobile-table">helpdesktech</td>
+			    				<td class="mobile-table">11/30/2015</td>
 			    				<td>12/2/2015</td>
 			    			</tr>
 			    			<tr>
 			    				<td><a href="?page=ViewRequests&sr=3">3</a></td>
-			    				<td>Software</td>
+			    				<td class="mobile-table">Software</td>
 			    				<td>Completed</td>
 			    				<td>TT</td>
-			    				<td>The Mac Admin</td>
-			    				<td>12/1/2015</td>
+			    				<td class="mobile-table">The Mac Admin</td>
+			    				<td class="mobile-table">12/1/2015</td>
 			    				<td>12/2/2015</td>
 			    			</tr>
 			    		</tbody>
