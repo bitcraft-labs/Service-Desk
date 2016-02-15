a25a460c084e5b0d9430c6df06aa02b8d9c4d3e3
D3Exclusive <eduffy2@student.fitchburgstate.edu>
2016-02-15 01:00:49 -0500

    Fixed deprecated tags and fixed tables and added javascript to handle events

--- modules/end_user_portal/mailbox.php
+++ modules/end_user_portal/mailbox.php
@@ -55,137 +55,137 @@
 		              <div class="table-responsive mailbox-messages">
 		                <table class="table table-hover table-striped">
 		                  <tbody>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"></td>
 		                    <td class="mailbox-date">5 mins ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">28 mins ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">11 hours ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"></td>
 		                    <td class="mailbox-date">15 hours ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">Yesterday</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">2 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">2 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"></td>
 		                    <td class="mailbox-date">2 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"></td>
 		                    <td class="mailbox-date">2 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"></td>
 		                    <td class="mailbox-date">2 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">4 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"></td>
 		                    <td class="mailbox-date">12 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star-o text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">12 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">14 days ago</td>
 		                  </tr>
-		                  <tr>
+		                  <tr data-href="?page=Mailbox&mb=<?= $read_comments_number++; ?>">
 		                    <td><input type="checkbox"></td>
 		                    <td class="mailbox-star"><a href="#"><i class="fa fa-star text-yellow"></i></a></td>
-		                    <td class="mailbox-name"><a href="?page=Mailbox&mb=<?php echo $read_comments_number++; ?>">Alexander Pierce</a></td>
-		                    <td class="mailbox-subject"><b>Computer Issue</b> - Trying to find a solution to this problem...
+		                    <td class="mailbox-name">Alexander Pierce</td>
+		                    <td class="mailbox-subject"><strong>Computer Issue</strong> - Trying to find a solution to this problem...
 		                    </td>
 		                    <td class="mailbox-attachment"><i class="fa fa-paperclip"></i></td>
 		                    <td class="mailbox-date">15 days ago</td>
