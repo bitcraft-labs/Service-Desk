71af8322e78b840d6a82ccad4ca01b4a443f8e15
FerosNS <aperry19@student.fitchburgstate.edu>
2016-02-15 19:56:48 -0500

    Modularized Active Users table

--- dashboard.php
+++ dashboard.php
@@ -50,62 +50,11 @@
 		</div>
 	</div>
 	<div class="row">
-		<?php include_once "modules/hd_portal/recent_act.php";?>
-		<div class="col-md-6 col-xs-12">
-			<div class="box">
-				<div class="box-header">
-				  <h3 class="box-title">Active Staff Users</h3>
-				</div><!-- /.box-header -->
-				<div class="box-body">
-					<table id="active_staff" class="table table-bordered table-striped">
-						<thead>
-						<?php $tabhead="
-							<tr>
-								<td>#</td>
-								<td>First Name</td>
-								<td>Last Name</td>
-								<td>User Group</td>
-								<td>Active SR #</td>
-								<td>Last Online</td>
-							</tr>"; echo $tabhead; ?>
-						</thead>
-						<tbody>
-							<?php /*
-							$dal = new DAL();
-							$personinfo = $dal->getPersonInfo('all');
-							if ($personinfo) {
-								foreach($personinfo as $row) {
-							    echo "<tr>
-								    	<td><a href='?id=$row->user_id'>$row->user_id</a></td>
-								    	<td>$row->name</td>
-								    	<td>@$row->banner_id</td>
-								    	<td>$row->user_type</td>
-								    	<td>$row->phone</td>
-								    	<td>$row->email</td>
-								    	<td>12/1/2015</td>
-								    	</tr>";
-							  	}
-							} */
-							?>
-							<!--
-							<tr>
-								<td><a href="?id=1">1</a></td>
-								<td>Jo Shmo</td>
-								<td>@01234567</td>
-								<td>Student</td>
-								<td>555.555.5555</td>
-								<td>jshmo1@student.fitchburgstate.edu</td>
-								<td>12/3/2015</td>
-							</tr>
-							-->
-						</tbody>
-						<tfoot>
-							<?php echo $tabhead; ?>
-						</tfoot>
-					</table>
-				</div>
-			</div>
-		</div>
+		<?php
+		include_once "modules/hd_portal/recent_act.php";
+		include_once "modules/hd_portal/act_user.php";
+		?>
+
 	</div>
 	</section><!-- /.content -->
 </div><!-- /.content-wrapper -->
