5357b48da854234602ee252411c732a2b8b54290
Joshua Nasiatka <joshuanasiatka@gmail.com>
2016-02-19 23:49:26 -0500

    Changed some formatting

--- modules/footer.php
+++ modules/footer.php
@@ -1,7 +1,8 @@
 <!-- Main Footer -->
 <footer class="main-footer">
 	  <div class="pull-right app-version-mobile">
+			<a href="http://itsm.bitcraftlabs.net" target="_blank">
 	    <strong>Version</strong> <?php echo $app_version; ?></a>
 	  </div>
-	  <strong>Copyright &copy; 2016 <a href="https://bitcraftlabs.net/" target="_blank">Bitcraft Labs</a>.</strong><span class="mobile-fix">All rights reserved.</span>
+	  <strong>Copyright &copy; 2016 <a href="https://bitcraftlabs.net/" target="_blank">Bitcraft Labs</a>. </strong><span class="mobile-fix">All rights reserved.</span>
 </footer>
