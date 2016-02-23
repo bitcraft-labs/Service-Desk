9e3f31d0a3254fc47610fc501972e5e39cbb95ba
FerosNS <aperry19@student.fitchburgstate.edu>
2016-02-23 09:46:45 -0500

    Added <section> to sr_new to separate content better

--- modules/service_record/templates/sr_new.php
+++ modules/service_record/templates/sr_new.php
@@ -5,80 +5,82 @@
 <style>.bootstrap-iso .formden_header h2, .bootstrap-iso .formden_header p, .bootstrap-iso form{font-family: Arial, Helvetica, sans-serif; color: black}.bootstrap-iso form button, .bootstrap-iso form button:hover{color: #ffffff !important;} .asteriskField{color: red;}.bootstrap-iso form .input-group-addon {color:#555555; background-color: #d6d6d6; border-radius: 4px; padding-left:12px}</style>
 
 <!-- HTML Form (wrapped in a .bootstrap-iso div) -->
-<div class="bootstrap-iso">
- <div class="container-fluid">
-  <div class="row">
-   <div class="col-md-6 col-sm-6 col-xs-12">
-    <form method="post">
-     <div class="form-group ">
-      <label class="control-label requiredField" for="sr_type">
-       Type of Service Record
-       <span class="asteriskField">
-        *
-       </span>
-      </label>
-      <select class="select form-control" id="sr_type" name="sr_type">
-       <option value="Incident">
-        Incident
-       </option>
-       <option value="Repair">
-        Repair
-       </option>
-       <option value="Other">
-        Other
-       </option>
-      </select>
-      <span class="help-block" id="hint_sr_type">
-       Select type of service record
-      </span>
+<section class="content">
+  <div class="bootstrap-iso">
+   <div class="container-fluid">
+    <div class="row">
+     <div class="col-md-6 col-sm-6 col-xs-12">
+      <form method="post">
+       <div class="form-group ">
+        <label class="control-label requiredField" for="sr_type">
+         Type of Service Record
+         <span class="asteriskField">
+          *
+         </span>
+        </label>
+        <select class="select form-control" id="sr_type" name="sr_type">
+         <option value="Incident">
+          Incident
+         </option>
+         <option value="Repair">
+          Repair
+         </option>
+         <option value="Other">
+          Other
+         </option>
+        </select>
+        <span class="help-block" id="hint_sr_type">
+         Select type of service record
+        </span>
+       </div>
+       <div class="form-group ">
+        <label class="control-label requiredField" for="user">
+         User
+         <span class="asteriskField">
+          *
+         </span>
+        </label>
+        <select class="select form-control" id="user" name="user">
+         <option value="Allen Perry">
+          Allen Perry
+         </option>
+         <option value="Eugene Duffy">
+          Eugene Duffy
+         </option>
+         <option value="Joshua Nasiatka">
+          Joshua Nasiatka
+         </option>
+        </select>
+        <span class="help-block" id="hint_user">
+         Select User
+        </span>
+       </div>
+       <div class="form-group ">
+        <label class="control-label requiredField" for="machine">
+         Machine
+         <span class="asteriskField">
+          *
+         </span>
+        </label>
+        <select class="select form-control" id="machine" name="machine">
+         <option value="Add New">
+          Add New
+         </option>
+        </select>
+        <span class="help-block" id="hint_machine">
+         Select user machine or add new
+        </span>
+       </div>
+       <div class="form-group">
+        <div>
+         <button class="btn btn-success " name="submit" type="submit">
+          Submit
+         </button>
+        </div>
+       </div>
+      </form>
      </div>
-     <div class="form-group ">
-      <label class="control-label requiredField" for="user">
-       User
-       <span class="asteriskField">
-        *
-       </span>
-      </label>
-      <select class="select form-control" id="user" name="user">
-       <option value="Allen Perry">
-        Allen Perry
-       </option>
-       <option value="Eugene Duffy">
-        Eugene Duffy
-       </option>
-       <option value="Joshua Nasiatka">
-        Joshua Nasiatka
-       </option>
-      </select>
-      <span class="help-block" id="hint_user">
-       Select User
-      </span>
-     </div>
-     <div class="form-group ">
-      <label class="control-label requiredField" for="machine">
-       Machine
-       <span class="asteriskField">
-        *
-       </span>
-      </label>
-      <select class="select form-control" id="machine" name="machine">
-       <option value="Add New">
-        Add New
-       </option>
-      </select>
-      <span class="help-block" id="hint_machine">
-       Select user machine or add new
-      </span>
-     </div>
-     <div class="form-group">
-      <div>
-       <button class="btn btn-success " name="submit" type="submit">
-        Submit
-       </button>
-      </div>
-     </div>
-    </form>
+    </div>
    </div>
   </div>
- </div>
-</div>
+</section><!-- /.content -->
