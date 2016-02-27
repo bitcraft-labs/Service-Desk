71156c632f9fadec664ea21351eb3748dc7887b0
Allen Perry <eternaldarkness10@gmail.com>
2016-02-26 23:19:04 -0500

    Updated things
    Make some css, modified authenticator to fix leftover id_user
    references, added profile-ui, and began work on Service Request forms.

--- modules/mainhead.php
+++ modules/mainhead.php
@@ -2,7 +2,7 @@
 include_once 'modules/config.inc.php';
 require_once("./modules/authentication/config.php");
 include_once 'modules/config-func.php';
-include("modules/authentication/database.php"); 
+include("modules/authentication/database.php");
 include("modules/authentication/class.acl.php");
 
 $dal = new DAL();
@@ -15,4 +15,7 @@ if(!$authenticator->CheckLogin())
     $authenticator->RedirectToURL("login.php");
     exit;
 }
+
+//who am i
+$whoami = $_SESSION['userID'];
 ?>
